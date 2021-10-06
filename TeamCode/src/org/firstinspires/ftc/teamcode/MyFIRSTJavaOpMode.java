cbb91036427f98892de0d2443e4acaae70c89df1
mhuynh5 <melvinhuynh5@gmail,com>
2021-10-05 17:26:33 -0700

    Did things

--- TeamCode/src/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -35,11 +35,29 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
+            motorTest.setPower(tgtPower);
+            // check to see if we need to move the servo.
+            if(gamepad1.y) {
+                // move to 0 degrees.
+                servoTest.setPosition(0);
+            } else if (gamepad1.x || gamepad1.b) {
+                // move to 90 degrees.
+                servoTest.setPosition(0.5);
+            } else if (gamepad1.a) {
+                // move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+            telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
+            // set digital channel to input mode.
+            digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
             telemetry.addData("Status", "Running");
             telemetry.update();
-
+            // Wait for the game to start (driver presses PLAY)
+            waitForStart();
         }
     }
 }
